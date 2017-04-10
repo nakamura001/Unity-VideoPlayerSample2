ee4f0ffd8a346b78b9d63aeeeed5fe412150bbd4
takashi nakamura <polluxtk@gmail.com>
2017-04-10 16:36:11 +0900

    aspectRatio 動作確認用のサンプルを追加

---
+++ Assets/Scripts/Test2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Test2 : MonoBehaviour {
+    public VideoPlayer videoPlayer;
+    public VideoClip[] videoClip;
+	public RenderTexture renderTexture;
+    int videoNo = 0;
+
+	public void Start() {
+		videoPlayer.clip = videoClip[0];
+	}
+
+    public void ChangeAspectRatio(string aspectRatio) {
+        switch (aspectRatio) {
+        case "NoScaling":
+            videoPlayer.aspectRatio = VideoAspectRatio.NoScaling;
+            break;
+        case "FitVertically":
+            videoPlayer.aspectRatio = VideoAspectRatio.FitVertically;
+            break;
+        case "FitHorizontally":
+            videoPlayer.aspectRatio = VideoAspectRatio.FitHorizontally;
+            break;
+        case "FitInside":
+            videoPlayer.aspectRatio = VideoAspectRatio.FitInside;
+            break;
+        case "FitOutside":
+            videoPlayer.aspectRatio = VideoAspectRatio.FitOutside;
+            break;
+        case "Stretch":
+            videoPlayer.aspectRatio = VideoAspectRatio.Stretch;
+            break;
+		}
+        videoPlayer.Play();
+	}
+
+    public void ChangeVideo() {
+        bool isPlaying = videoPlayer.isPlaying;
+        videoNo = (videoNo+1) % 2;
+        if (isPlaying) {
+            videoPlayer.Stop();
+        }
+        videoPlayer.clip = videoClip[videoNo];
+        if (isPlaying) {
+            videoPlayer.Play();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Unity-VideoPlayerSample2
 Unity の VideoPlayer のサンプル
+
+※プログラム内では使ってないですが検証用に sample_trans.webm と sample4k.mp4 を同梱しています。
